64da3521070ffaa6bea8d84438a4d88d503c276c
Foad Karimian <37878164+foadkrmn@users.noreply.github.com>
2018-04-05 15:12:45 -0400

    Add files via upload

---
+++ Nelder_Mead_approach.m
@@ -0,0 +1,48 @@
+% Applying Nelder-Mead method to find Lagrange Multiplyers to generate distribution with hieghst
+% entrpy with constraint. 
+% This code is developed by Foad Karimian.
+
+clear all;
+clc;
+
+xmin=-2;
+xmax=2;
+dx=0.01;
+x=[xmin:dx:xmax];
+mu=[0 0.2 0.05 0.1];
+
+mu=mu(:);                    %import mu and make a vector
+x=x(:);                      %make a vector of x
+M=length(mu);                %determines sumation over indicies
+phi=ones(length(x),M);       %function to generate moments (mean, variance,...)
+phi(:,1)=phi(:,1).*x;        %first column is x
+
+for i=2:M
+    phi(:,i)=phi(:,i-1).*x;  %generate x^i
+end
+
+phmu=zeros(length(x),M);
+
+for i=1:M
+    phmu(:,i)=phi(:,i)-mu(i);       %generates x^i - mu(i)
+end
+
+l0=zeros(M,1);
+
+Q = @(l) sum(exp(-phmu*l).*dx)
+
+options = optimset('Display','iter','PlotFcns',@optimplotfval);
+lambda = fminsearch(Q,l0,options)  %minimizing potential function to find Lagrangian multiplyers
+
+q = sum(exp(-phmu*lambda).*dx);     %calculate potential value
+
+p=exp(-phmu*lambda)./q;             %generate distribution
+
+lambda0=sum(exp(-phi*lambda).*dx);  %find lambda0, normalizing factor
+
+lambda=[lambda0;lambda];
+lambda
+
+plot(p)
+
+    
