de17deab78c186a908a9e79c0b253b3d78b891aa
Foad Karimian <37878164+foadkrmn@users.noreply.github.com>
2018-04-06 15:33:03 -0400

    Update Nelder_Mead_approach.m

--- Nelder_Mead_approach.m
+++ Nelder_Mead_approach.m
@@ -1,15 +1,20 @@
 % Applying Nelder-Mead method to find Lagrange Multiplyers to generate distribution with hieghst
 % entrpy with constraint. 
 % This code is developed by Foad Karimian.
+% To verify the code, following data can be used based on example provided
+% in Mohammad-Djafari, A. (1992). A Matlab program to calculate the maximum entropy 
+% distributions: if x=[-1:0.01:1], mu=[0 0.3 0 0.15], then independent of
+% initial value for lambda, final lambda values should be lambda=[0.9392 0
+% -3.3414 0 4.6875].
 
 clear all;
 clc;
 
-xmin=-3;
-xmax=3;
-dx=0.001;
+xmin=-6;
+xmax=6;
+dx=0.01;
 x=[xmin:dx:xmax];
-mu=[0 1 0.2884 1.9006];
+mu=[0 1];
 
 mu=mu(:);                    %import mu and make a vector
 x=x(:);                      %make a vector of x
@@ -36,11 +41,23 @@ lambda = fminsearch(Q,l0,options);  %minimizing potential function to find Lagra
 
 q = sum(exp(-phmu*lambda).*dx);     %calculate potential value
 
-p=exp(-phmu*lambda)./q;             %generate distribution
+pdf=exp(-phmu*lambda)./q;             %generate distribution  
 
-lambda0 = log(sum(exp(-(phi*lambda)).*dx));  %find lambda0, normalizing factor
+lambda0 = log(q.*exp(-lambda.'*mu));    %find lambda0, normalizing factor
 
-lambda=[lambda0;lambda];
-lambda
+lambda=-lambda;
 
-plot(x,p)
+lambda=[lambda0;lambda]
+
+figure(1)
+plot(x,pdf)
+
+cdf=zeros(length(x),1);
+cdf(1)=pdf(1);
+
+for i=2:length(x)
+    cdf(i)=pdf(i)+cdf(i-1);
+end
+
+figure(2)
+plot(x,cdf)
